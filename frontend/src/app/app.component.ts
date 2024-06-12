c7835f158a9b793801fefa4efcbcc5f6f6c8fb62
Alexandra Rusak <alexandra.myagkova@gmail.com>
2024-06-12 22:24:36 +0300

    Add oauth methods

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import {CommonModule} from "@angular/common";
+import {DashboardComponent} from "./components/dashboard/dashboard.component";
+import {NavbarComponent} from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [CommonModule, RouterOutlet, LoginComponent, DashboardComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from "./components/login/login.component";
+import {DashboardComponent} from "./components/dashboard/dashboard.component";
+import {Dashboard2Component} from "./components/dashboard2/dashboard2.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'googleOauth2callback', component: DashboardComponent},
+  {path: 'yandexOauth2callback', component: Dashboard2Component},
+];
